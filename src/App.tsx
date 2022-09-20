4bb7d960ecc5ec8cf54e70475f8eaf84bef12325
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-09-20 09:44:08 -0300

    continuacao do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
   return (
-    <div></div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
+
+  // <GlobalStyle /> pode ficar em qualquer lugar, mas é interessante que fique dentro do ThemeProvider
+  //                 para acessar o que existe dentro
 }
