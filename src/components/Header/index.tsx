f243297f1862f9ea05e9fddcb7902a1bc1a205d1
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-09-22 18:24:02 -0300

    contrução da home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { HeaderContainer } from "./styles";
 import logoIgnite from "../../assets/logo-ignite.svg";
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
     return (
         <HeaderContainer>
             <img src={logoIgnite} alt="" />
             <nav>
-                <a href="">Timer</a>
-                <a href="">Histórico</a>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/history" title="Histórico">
+                    <Scroll size={24} />
+                </NavLink>
             </nav>
         </HeaderContainer>
     );
