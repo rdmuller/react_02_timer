ea37b97aaaea474b45c2aa2a90168620b637c7a6
Ronald Muller <ronald.muller@silsistemas.com.br>
2022-10-17 12:46:03 -0300

    immer

--- src/reducers/cycle/reducer.ts
+++ src/reducers/cycle/reducer.ts
@@ -1,4 +1,5 @@
 import { ActionTypes } from "./actions";
+import { produce } from "immer";
 
 interface CycleState {
     cycles: Cycle[];
@@ -17,36 +18,56 @@ export interface Cycle {
 export function cyclesReducer(state: CycleState, action: any) {
     switch (action.type) {
         case ActionTypes.ADD_NEW_CYCLE:
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id,
+            // return {
+            //     ...state,
+            //     cycles: [...state.cycles, action.payload.newCycle],
+            //     activeCycleId: action.payload.newCycle.id,
+            // }
+            return produce(state, draft => {
+                draft.cycles.push(action.payload.newCycle);
+                draft.activeCycleId = action.payload.newCycle.id;
+            })
+        case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+            // return {
+            //     ...state,
+            //     activeCycleId: null,
+            //     cycles: state.cycles.map((cycle) => {
+            //         if (cycle.id != state.activeCycleId) {
+            //             return cycle;
+            //         } else {
+            //             return { ...cycle, interruptedDate: new Date() }
+            //         }
+            //     }),
+            // }
+
+            const currentCycleIndex = state.cycles.findIndex((cycle) => {
+                return cycle.id === state.activeCycleId
+            });
+
+            if (currentCycleIndex < 0) {
+                return state;
+            } else {
+                return produce(state, draft => {
+                    draft.activeCycleId = null;
+                    draft.cycles[currentCycleIndex].interruptedDate = new Date();
+                });
             }
-        case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-            return {
-                ...state,
-                activeCycleId: null,
-                cycles: state.cycles.map((cycle) => {
-                    if (cycle.id != state.activeCycleId) {
-                        return cycle;
-                    } else {
-                        return { ...cycle, interruptedDate: new Date() }
-                    }
-                }),
+        }
+        case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+            const currentCycleIndex = state.cycles.findIndex((cycle) => {
+                return cycle.id === state.activeCycleId
+            });
+
+            if (currentCycleIndex < 0) {
+                return state;
+            } else {
+                return produce(state, draft => {
+                    draft.activeCycleId = null;
+                    draft.cycles[currentCycleIndex].finishedDate = new Date();
+                });                
             }
-        case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-            return {
-                ...state,
-                activeCycleId: null,
-                cycles: state.cycles.map((cycle) => {
-                    if (cycle.id != state.activeCycleId) {
-                        return cycle
-                    } else {
-                        return { ...cycle, finishedDate: new Date() }
-                    }
-                })
-            };
-        default:
+        }
+       default:
             console.log("Opção inválida [cyclesState, dispatch] = useReducer((state: CycleState, action: any)")
             return state;
     }
